21f844e14356e08fb654e55e6cac6734f3bd3f8a
brandienglish <brandienglish87@gmail.com>
2024-02-04 11:21:49 -0600

    update

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = () => (
+  <div className='"w-full flex justiy-center intems-center'>
+    <h1 className='font-bold text-2x1 text-white mt-2'>opps error try again </h1>
+  </div>
+);
+
+export default Error;
